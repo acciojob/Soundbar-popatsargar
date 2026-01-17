ea5c177cabe4a5d2a75d976a5e1fc0446e203915
Popat Bhausaheb Sargar <sargarpopat219@gmail.com>
2026-01-17 14:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const soundName = button.getAttribute("data-sound");
+        playSound(soundName);
+    });
+});
+
+stopButton.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
+
+function playSound(sound) {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+
+    currentAudio = new Audio(`./sounds/${sound}.mp3`);
+    currentAudio.play();
+}
